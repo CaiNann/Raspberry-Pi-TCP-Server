f900c336750af661b7ecfe7adce656af62bb2086
Cainan <cainanenneking2@gmail.com>
2025-10-20 16:50:02 -0700

    Added file upload capability from client

--- client.c
+++ client.c
@@ -5,7 +5,7 @@
 #include <netinet/in.h>
 #include <sys/types.h>
 
-int main(void) {
+int main(int argc, char *args[]) {
 	int client_fd = socket(PF_INET, SOCK_STREAM, 0);
 
 	struct sockaddr_in address;
@@ -17,8 +17,22 @@ int main(void) {
 	connect(client_fd, (struct sockaddr*)&address, (socklen_t)sizeof(address));
 	printf("Successfully connected to server...\n");
 
-	char *message = "Hello Server!\n";
-	write(client_fd, message, strlen(message));
+	if (argc == 1) {
+		char *message = "Hello Server!\n";
+		write(client_fd, message, strlen(message));
+	} else {
+		for (int i = 1; i < argc; i++) {
+			char buffer[1024];
+			memset(&buffer, 0, sizeof(buffer));
+			int input_fd = open(args[i], O_RDONLY);
+			while (size_t r_bytes = read(input_fd, buffer, (size_t)1024) != 0) {
+				printf("Read %d bytes from %s\n", r_bytes, args[i]);
+				int w_bytes = write(client_fd, buffer, bytes);
+				printf("Wrote %d bytes to server\n", w_bytes);
+			}
+			close(input_fd);
+		}
+	}
 
 	close(client_fd);
 	printf("Closed connection to server.\n");
