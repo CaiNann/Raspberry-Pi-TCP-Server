d7b4876c86d7401428d035b7ba5b1b35aae747fe
Cainan <cainanenneking2@gmail.com>
2025-10-21 09:41:54 -0700

    Improved error checking

--- client.c
+++ client.c
@@ -9,7 +9,6 @@
 #include <sys/types.h>
 
 int main(int argc, char *args[]) {
-	printf("argc: %d", argc);
 	int client_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (client_fd < 0) {
 		perror("Socket failed");
@@ -23,23 +22,46 @@ int main(int argc, char *args[]) {
 	address.sin_addr.s_addr = inet_addr("192.168.1.134");
 
 	int	connection = connect(client_fd, (struct sockaddr*)&address, (socklen_t)sizeof(address));
-	printf("Connect return code: %d", connection);
-	printf("Error code: %s (errno: %d)", strerror(errno), errno);
+	if (connection < 0) {
+		perror("Socket failed");
+		return 1;
+	}
 	printf("Successfully connected to server...\n");
 
 	if (argc == 1) {
-		printf("Writing 'Hello Server!'");
 		char *message = "Hello Server!\n";
-		write(client_fd, message, strlen(message));
+		size_t w_bytes = write(client_fd, message, strlen(message));
+
+		if (w_bytes < 0) {
+			perror("Write failed");
+			return 1;
+		}
 	} else {
 		for (int i = 1; i < argc; i++) {
 			char buffer[1024];
 			memset(&buffer, 0, sizeof(buffer));
+
 			int input_fd = open(args[i], O_RDONLY);
+			if (input_fd < 0) {
+				perror("Failed to open file");
+				return 1;
+			}
+
 			size_t r_bytes;
 			while ((r_bytes = read(input_fd, buffer, (size_t)1024)) != 0) {
+				if (r_bytes < 0) {
+					perror("Failed to read from file");
+					return 1;
+				}
+
 				printf("Read %zu bytes from %s\n", r_bytes, args[i]);
+
 				int w_bytes = write(client_fd, buffer, r_bytes);
+				if (w_bytes < 0) {
+					perror("Failed to write buffer to server");
+					return 1;
+				}
+
 				printf("Wrote %d bytes to server\n", w_bytes);
 			}
 			close(input_fd);
--- server.c
+++ server.c
@@ -1,4 +1,5 @@
 #include <sys/socket.h>
+#include <errno.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <unistd.h>
@@ -8,35 +9,49 @@
 int main(void) {
 
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd < 0) {
+		perror("Server socket failed");
+		return 1;
+	}
 
 	struct sockaddr_in address;
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY;  // Bind socket with
-													   // localhost
 	address.sin_port = htons(8080);        // Port 8080
 
 	int	addr_bind = bind(server_fd, (struct sockaddr*)&address, (socklen_t)sizeof(address));
-
 	if (addr_bind < 0) {
 		perror("bind failed");
 		return 1;
 	}
 
-	listen(server_fd, 3);
+	int listener = listen(server_fd, 3);
+	if (listener < 0) {
+		perror("Listen failed");
+		return 1;
+	}
+
 	printf("Server is now listening on port %d...\n", ntohs(address.sin_port)); 
 
 	for(;;) {
 		struct sockaddr_in client_addr;
 		socklen_t client_len = sizeof(client_addr);
+
 		int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
 		if (client_fd < 0) {
  	    	perror("accept failed");
     		return 1;
 		}
+
 		printf("Connection accepted!\n");
 
 		char buffer[1024] = {0};
 		size_t bytes = read(client_fd, buffer, 1024);
+		if (bytes < 0) {
+			perror("Read failed");
+			return 1;
+		}
+
 		printf("%d bytes read from client: %s\n", bytes, buffer);
 
 		close(client_fd);
