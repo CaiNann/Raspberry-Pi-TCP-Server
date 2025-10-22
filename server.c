434e8b63391aab4bdf8beefc43c61efbe6db60a6
Cainan <cainanenneking2@gmail.com>
2025-10-22 00:46:36 -0700

    begin working on password authentication.

--- server.c
+++ server.c
@@ -1,4 +1,5 @@
 #include <sys/socket.h>
+#include <openssl/sha.h>
 #include <errno.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -8,6 +9,41 @@
 
 int main(void) {
 
+	int server_fd = start_server();
+
+	for(;;) {
+		struct sockaddr_in client_addr;
+		socklen_t client_len = sizeof(client_addr);
+
+		int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
+		if (client_fd < 0) {
+ 	    	perror("accept failed");
+    		return 1;
+		}
+
+		check_password();
+
+		printf("Connection accepted!\n");
+
+		char buffer[1024] = {0};
+		size_t bytes = read(client_fd, buffer, 1024);
+		if (bytes < 0) {
+			perror("Read failed");
+			return 1;
+		}
+
+		printf("%d bytes read from client: %s\n", bytes, buffer);
+
+		close(client_fd);
+	}
+
+	close(server_fd);
+
+	return 0;
+}
+
+int start_server(void) {
+	
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd < 0) {
 		perror("Server socket failed");
@@ -32,32 +68,9 @@ int main(void) {
 	}
 
 	printf("Server is now listening on port %d...\n", ntohs(address.sin_port)); 
+	return server_fd;
+}
 
-	for(;;) {
-		struct sockaddr_in client_addr;
-		socklen_t client_len = sizeof(client_addr);
-
-		int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
-		if (client_fd < 0) {
- 	    	perror("accept failed");
-    		return 1;
-		}
-
-		printf("Connection accepted!\n");
-
-		char buffer[1024] = {0};
-		size_t bytes = read(client_fd, buffer, 1024);
-		if (bytes < 0) {
-			perror("Read failed");
-			return 1;
-		}
-
-		printf("%d bytes read from client: %s\n", bytes, buffer);
-
-		close(client_fd);
-	}
-
-	close(server_fd);
-
-	return 0;
+int check_password(char* password) {
+	int pass_file = open("pass.txt", O_RDONLY);
 }
